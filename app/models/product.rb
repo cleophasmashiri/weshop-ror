c61a69037e1ebb55de85b65e15a5c45339cc90e8
Cleophas Mashiri <cleophasmashiri@gmail.com>
2015-04-28 09:44:05 +0200

    fix index styling

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "400x600#", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "400x600#", :thumb => "320x150>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_many :purchases
   has_many :users, through: :purchases
